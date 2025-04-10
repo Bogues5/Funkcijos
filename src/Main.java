64ee491875566fd3e1f8148610d92c7eb01a87a1
Bogues5 <suverenas@tutanota.com>
2025-04-10 11:46:49 +0300

    commit continuesly

--- src/Main.java
+++ src/Main.java
@@ -39,6 +39,21 @@ public class Main {
 
         System.out.println("Funkcijos užduotis 13");
         testGenerateRndStr();
+
+        System.out.println("Funkcijos užduotis 14");
+        testCountDivisors();
+
+        System.out.println("Funkcijos užduotis 15");
+        testSortByDivisors();
+
+        System.out.println("Funkcijos užduotis 16");
+        testCountPrimes();
+
+        System.out.println("Funkcijos užduotis 17");
+        testAddUntilAllPrimes();
+
+        System.out.println("Funkcijos užduotis 18");
+        testAdjustUntilPrimeAverage();
     }
 
     // 1 užduotis: Funkcija suma
@@ -168,7 +183,7 @@ public class Main {
 
     // 13 užduotis: Funkcija, kuri generuoja atsitiktinį tekstą
     static String generateRndStr(int length) {
-        String symbols = "1T5GHLKMPB";
+        String symbols = "[159GHLKMP]";
         String text = "";
         for (int i = 0; i < length; i++) {
             text += symbols.charAt((int) (Math.random() * symbols.length()));
@@ -180,36 +195,167 @@ public class Main {
         char numberToBracket = '5'; // Pasirenkame skaičių, kurį apgaubsime skliaustais (pvz., 5)
         for (int i = 0; i < randomText.length(); i++) {
             char currentChar = randomText.charAt(i);
-            // Tikriname, ar simbolis yra skaičius ir ar jis yra tas, kurį norime apgaubti
             if (Character.isDigit(currentChar) && currentChar == numberToBracket) {
                 System.out.println("[" + currentChar + "]"); // Apgaubiame skliaustais
             } else {
                 System.out.println(currentChar); // Spausdiname be skliaustų
             }
         }
+    }
 
+    // 14 užduotis: Funkcija, kuri skaičiuoja daliklių be liekanos kiekį (išskyrus 1 ir patį skaičių)
+    static int countDivisors(int number) {
+        if (number <= 1) return 0; // Skaičiai <= 1 neturi daliklių pagal sąlygą
+        int count = 0;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+    static void testCountDivisors() {
+        System.out.println(countDivisors(12)); // Testuojame su 12 (dalikliai: 2, 3, 4, 6 → 4 dalikliai)
+    }
 
+    // 15 užduotis: Masyvas iš 100 elementų (33–77), rūšiuojamas pagal daliklių kiekį mažėjimo tvarka
+    static void sortByDivisors(int[] arr) {
+        // Rūšiuojame masyvą pagal daliklių kiekį mažėjimo tvarka
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (countDivisors(arr[j]) < countDivisors(arr[j + 1])) {
+                    // Keičiame vietomis
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    static void testSortByDivisors() {
+        int[] array = randomArray(33, 77, 100); // Generuojame masyvą
+        sortByDivisors(array); // Rūšiuojame pagal daliklių kiekį
+        printArray(array); // Spausdiname išrūšiuotą masyvą
+    }
 
+    // Pagalbinė funkcija: Tikrina, ar skaičius pirminis
+    static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
 
+    // 16 užduotis: Masyvas iš 100 elementų (333–777), suskaičiuoti pirminius skaičius
+    static int countPrimes(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (isPrime(num)) count++;
+        }
+        return count;
+    }
+    static void testCountPrimes() {
+        int[] array = randomArray(333, 777, 100); // Generuojame masyvą
+        System.out.println("Pirminių skaičių kiekis: " + countPrimes(array));
+    }
 
+    // 17 užduotis: Masyvas iš 3 elementų (1–33), pridedame elementus, kol visi pirminiai
+    static int[] addUntilAllPrimes() {
+        int[] array = randomArray(1, 33, 3); // Pradinis masyvas iš 3 elementų
+        while (true) {
+            boolean allPrime = true;
+            // Tikriname paskutinius 3 elementus
+            int start = Math.max(0, array.length - 3);
+            for (int i = start; i < array.length; i++) {
+                if (!isPrime(array[i])) {
+                    allPrime = false;
+                    break;
+                }
+            }
+            if (allPrime) break; // Jei visi pirminiai, baigiame
+            // Pridedame naują elementą
+            int[] newArray = new int[array.length + 1];
+            System.arraycopy(array, 0, newArray, 0, array.length);
+            newArray[array.length] = randomNumber(1, 33);
+            array = newArray;
+        }
+        return array;
+    }
+    static void testAddUntilAllPrimes() {
+        int[] result = addUntilAllPrimes();
+        printArray(result); // Spausdiname galutinį masyvą
+    }
 
+    // 18 užduotis: 10x10 masyvas (1–100), didiname mažiausią skaičių, kol pirminių vidurkis ≥ 70
+    static void adjustUntilPrimeAverage(int[][] matrix) {
+        while (true) {
+            // Skaičiuojame pirminių skaičių sumą ir kiekį
+            int primeSum = 0;
+            int primeCount = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (isPrime(matrix[i][j])) {
+                        primeSum += matrix[i][j];
+                        primeCount++;
+                    }
+                }
+            }
+            // Skaičiuojame pirminių skaičių vidurkį
+            double primeAverage = primeCount == 0 ? 0 : (double) primeSum / primeCount;
+            if (primeAverage >= 70) break; // Jei vidurkis ≥ 70, baigiame
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            // Randame mažiausią skaičių ir jo poziciją
+            int min = Integer.MAX_VALUE;
+            int minI = 0, minJ = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (matrix[i][j] < min) {
+                        min = matrix[i][j];
+                        minI = i;
+                        minJ = j;
+                    }
+                }
+            }
+            // Pridedame 3 prie mažiausio skaičiaus
+            matrix[minI][minJ] += 3;
+        }
+    }
+    static void testAdjustUntilPrimeAverage() {
+        // Generuojame 10x10 masyvą
+        int[][] matrix = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            matrix[i] = randomArray(1, 100, 10);
+        }
+        adjustUntilPrimeAverage(matrix); // Koreguojame masyvą
+        // Spausdiname galutinį masyvą
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
